377eb7c6146393dbd69bf4ba9d4adc4d603de30a
Patricio Maturana <patricio.maturana.pardo@gmail.com>
2023-09-10 06:04:34 -0300

    Nueva versión, Pre-entrega 3: se realiza mejoras con el selector para los temas

--- scss/js/modificaTema.js
+++ scss/js/modificaTema.js
@@ -1,3 +1,4 @@
+/*
 const btnlight=document.querySelector(".btn-light");
 const btndark=document.querySelector(".btn-dark");
 
@@ -15,6 +16,23 @@ function poner_modo_dark(){
 function guardarCambio(tema){
     document.documentElement.setAttribute("tema-defecto",tema);
 }
+*/
+let checkbox = document.querySelector("input[name=theme_switch]");
 
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  checkbox.checked = false;
+}
+
+// switch theme if checkbox is engaged
+checkbox.addEventListener("change", (cb) => {
+  document.documentElement.setAttribute(
+    "data-theme",
+    cb.target.checked ? "dark" : "light"
+  );
+});
 
 
